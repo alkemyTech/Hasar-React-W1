795b4e7d585c9185103dfe8fbcb7c3a6cd5c1455
Leandro Mendoza <leandro.mendoza@hasar.com>
2023-02-28 02:14:20 -0300

    Creacion de Footer v2

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,11 @@
 import React from 'react'
+import './Footer.css';
 
 export const Footer = () => {
-  return (
-    
+  return ( 
 <div class="container">
-  <footer  class="footer"
-    dark
-    height="auto"
-  >
-      <p>
-        &copy;2023 — <strong>Marcelo Carabajal, Diego Diaz, Leandro Mendoza </strong>
-      </p>
-    
+  <footer  class="footer" dark height="auto">
+      <p>&copy;2023 — <strong>Marcelo Carabajal, Diego Diaz, Leandro Mendoza </strong></p>
   </footer>
-
 </div>
-  )
-}
+  )}
