c8aad9a0796296f8bf0a6b2fb6e8a5b8f38f5aa5
diegodiazdev7 <diego.diaz@hasar.com>
2023-03-01 13:44:41 -0300

    creacion de componente title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-export const Title = ({size, text}) => {
+export const Title = ({ size, text, children }) => {
+
+
+
+
   return (
     <>
-        
+      <Typography component="div">
+        <Box sx={{ fontSize: `${size}.fontSize`, m: 1 }}>
+          {text}
+          {children}
+        </Box>
+      </Typography>
     </>
   )
 }
