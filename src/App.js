fe57c1417e204cad0e707cec5adb822953234def
diegodiazdev7 <diego.diaz@hasar.com>
2023-03-04 11:46:30 -0300

    Create redux structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
+import { Provider as ReduxProvider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={'./alkemy_logo.svg'} className="App-logo" alt="logo" />
-        <p>
-          Bienvenido a AlkyBank
-        </p>
-      </header>
-    </div>
+    <ReduxProvider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={'./alkemy_logo.svg'} className="App-logo" alt="logo" />
+          <p>
+            Bienvenido a AlkyBank
+          </p>
+        </header>
+      </div>
+    </ReduxProvider>
   );
 }
 
