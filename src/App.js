4a4b4eddd1f30cc1a11aba3c4797e2661e294db1
MarceCarabajal <marcelo.carabajal@hasar.com>
2023-03-06 19:46:28 -0300

    transactions v1.2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
-import { Provider } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
 import './App.css';
 import Movimientos from './pages/Movimientos';
+import { increment, decrement, incrementBy } from './store/slices/counter/counterSlice';
+
+//prueba para enviar obj por props a Movimientos
+const transactions = [
+  {
+    id: 1,
+    date : '07/06/2023',
+    description: 'Varios',
+    amount: 4500,
+    balance: 5000
+  },
+  {
+    id: 2,
+    date : '07/06/2023',
+    description: 'Varios',
+    amount: 5000,
+    balance: 9500
+  },
+  {
+    id: 3,
+    date : '06/06/2023',
+    description: 'Varios',
+    amount: 500,
+    balance: 10000
+  },
+]
+
 
 
 function App() {
+
+  const { counter } = useSelector( state => state.counter)
+  const dispatch = useDispatch();
+
   return (
-    <Provider>
+
     <div className="App">
       <header className="App-header">
-        <Movimientos />
+        <Movimientos transactions={ transactions }/>
+        <p>Probando Redux - count is: {counter}</p>
+        <button type="button" onClick = { () => dispatch( increment() ) }>
+          increment
+        </button>
+        <button type="button" onClick = { () => dispatch( decrement() ) }>
+          decrement
+        </button>
+        <button type="button" onClick = { () => dispatch( incrementBy(3) ) }>
+          increment by 3
+        </button>
       </header>
     </div>
-    </Provider>
+
   );
 }
 
--- src/pages/Movimientos.jsx
+++ src/pages/Movimientos.jsx
@@ -1,44 +1,37 @@
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { fetchTransactions } from '../actions/transactions';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-// const Movimientos = () => {
-//   const dispatch = useDispatch();
-//   const transactions = useSelector(state => state.transactions);
 
-//   useEffect(() => {
-//     dispatch(fetchTransactions());
-//   }, []);
-
-//   return (
-//     <div>
-//       <h2>Movimientos</h2>
-//       <ul>
-//         {transactions.map(transaction => (
-//           <li key={transaction.id}>
-//             <span>{transaction.date}</span>
-//             <span>{transaction.description}</span>
-//             <span>{transaction.amount}</span>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Movimientos;
-
-//provisorio
-
-import { useSelector } from 'react-redux';
-import { mySlice } from '../mySlice'
-
-const Movimientos = () => {
-  const mySliceState = useSelector((state) => state.mySlice);
-
+const Movimientos = (props) => {
   return (
-    <div>Movimientos</div>
-  )
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Balance</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.transactions.map((transaction) => (
+            <TableRow key={transaction.id}>
+              <TableCell>{transaction.date}</TableCell>
+              <TableCell>{transaction.description}</TableCell>
+              <TableCell>{transaction.amount}</TableCell>
+              <TableCell>{transaction.balance}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
-export default Movimientos
+export default Movimientos;
