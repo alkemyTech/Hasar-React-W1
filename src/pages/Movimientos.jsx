78f1d9b00dbc29402d068a94d3f40a654735e813
MarceCarabajal <marcelo.carabajal@hasar.com>
2023-03-09 13:50:48 -0300

    transactions v2

--- src/pages/Movimientos.jsx
+++ src/pages/Movimientos.jsx
@@ -5,11 +5,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
 
 
 const Movimientos = (props) => {
   return (
     <TableContainer component={Paper}>
+      <Typography component='h1' style={{fontSize: 30}}>MOVIMIENTOS</Typography>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
